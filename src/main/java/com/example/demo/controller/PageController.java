1e29d26797ba0026742d567b911714b85dfa7d3e
nurul.aisyah61 <aisynurul99@gmail.com>
2018-09-22 11:21:12 +0700

    do tutorial-2

---
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+
+
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/viral")
+	public String index() {
+		return "viral";
+	}
+	
+	/*@RequestMapping("/challenge")
+	public String challenge(@RequestParam(value = "name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "challenge";
+	}*/
+	
+	@RequestMapping(value = {"/challenge", "challenge/{name}"})
+	public String challengePath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		}else {
+			model.addAttribute("name", "KB");
+		}
+		return "challenge";
+	}
+	
+	@RequestMapping("/generator")
+	public String generalPath(@RequestParam(value = "a", required = false, defaultValue = "0") Integer a, @RequestParam(value = "b", required = false, defaultValue = "0") Integer b, Model model) {
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		if(a <= 1 && b <= 1) {
+			model.addAttribute("result", "hm");
+		}else {
+			String temp = "h";
+			for (int i = 1; i < a; i++) {
+				temp += "m";
+			}
+			temp += "m";
+			String result = temp;
+			for (int i = 1; i < b; i++) {
+				result += " " + temp;
+			}
+			model.addAttribute("result", result);
+		}
+		return "generator";
+	}
+}
